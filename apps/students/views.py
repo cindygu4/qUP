4036b71ec33d48b26810357e2d81ba39876f357f
Cindy <cindygu@umich.edu>
2020-08-13 11:24:00 -0400

    added decorator for is_student/is_teacher

--- apps/students/views.py
+++ apps/students/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
+def is_student(user):
+    return user.is_student
+
 @login_required
+@user_passes_test(is_student)
 def index(request):
     return render(request, "students/index.html")
--- apps/teachers/views.py
+++ apps/teachers/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
+def is_teacher(user):
+    return user.is_teacher
+
 @login_required
+@user_passes_test(is_teacher)
 def index(request):
     return render(request, "teachers/index.html")
