60ff4f8c1f5604a427c53b805dbe6c356a0ff8c7
Cindy <cindygu@umich.edu>
2020-08-12 23:51:48 -0400

    added landing page and layouts of login and register pages

---
+++ apps/users/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+USER_CHOICES = (
+    ("Student", "Student"),
+    ("Instructor", "Instructor")
+)
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=64)
+    last_name = forms.CharField(max_length=64)
+    email = forms.EmailField()
+    user_type = forms.ChoiceField(choices=USER_CHOICES, label="I am a:")\
+
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.username = self.cleaned_data["username"]
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        if self.cleaned_data["user_type"] == "Instructor":
+            user.is_teacher = True
+        else:
+            user.is_student = True
+        if commit:
+            user.save()
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email addresses must be unique.')
+        return email
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.http import Http404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.views.decorators.csrf import csrf_exempt
+
+import operator
+import json
+from .forms import RegistrationForm
+from .models import User, Teacher, Student
 
 # Create your views here.
 def index(request):
     return render(request, "users/index.html")
+
+'''Allowing students and teachers to login'''
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is None:
+                messages.error(request, 'Invalid username and/or password.')
+            else:
+                login(request, user)
+                # redirect student and teacher to right pages
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {
+        'form': form
+    })
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:index')
+
+'''Allowing students and teachers to register'''
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            if user.is_teacher:
+                Teacher.objects.create(user=user)
+                # redirect teacher to right place
+            else:
+                Student.objects.create(user=user)
+        else:
+            return render(request, "users/register.html", {'form': form})
+    else:
+        form = RegistrationForm()
+    return render(request, "users/register.html", {
+        'form': form
+    })
